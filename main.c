cff658281e9c59cfdfd6643e150581b4a89f9942
alvinbutuba77 <90700365+alvinbutuba77@users.noreply.github.com>
2021-10-12 14:19:20 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+//a program to display the menu of
+// a library management system.
+int main()
+{
+    printf("Welcome to County Online Library!\n ");
+    printf("\n Options:\n");
+
+    puts("Sign up");
+    puts("Sign in");
+    puts("Login as guest user");
+
+    printf("\n Action:\n");
+    printf("\n 1. View Catalogue.");
+    printf("\n 2. Select category.");
+    printf("\n 3. View books.");
+    printf("\n 4. Add book. ");
+    printf("\n 5. Update catalogue.");
+    printf("\n 6. Return book.");
+
+
+}
